2828844fe665af2f0e85a6788e34f74d7c89165a
Ming Creekmore <mcreekmore480@gmail.com>
2022-09-25 14:16:31 -0400

    Updated Jersey documentation

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Jersey.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Jersey.java
@@ -3,9 +3,20 @@ import java.util.logging.Logger;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
+/**
+ * A representation of a Jersey
+ * @author Maple Leaves Team SWEN 2022
+ */
 public class Jersey {
     private static final Logger LOG = Logger.getLogger(Jersey.class.getName());
+    
+    /*
+     * enum to represent the size of a jersey
+     * 0: SMALL
+     * 1: Medium
+     * 3: Large
+     * 4: xL
+     */
     public enum Size {
         SMALL, MEDIUM, LARGE, XL;
     }
@@ -17,7 +28,18 @@ public class Jersey {
     @JsonProperty("home") private boolean isHome;
     @JsonProperty("number") private int number;
 
-    //The constructor for the Jersey
+    /**
+     * Create a jersey with a given id, name, cost, size, isHome, number
+     * @param id The id of a jersey
+     * @param name the player name on the jersey
+     * @param cost the cost of the jersey
+     * @param size the size of the jersey
+     * @param isHome if jersey is in colors isHome or Away
+     * @param number player number on the jersey
+     * 
+     * @literal @JsonProperty is used to deserialize and serialize JSON objects
+     *  to java objects in mapping the fields
+     */
     public Jersey(@JsonProperty("id") int id, @JsonProperty("name") String name, @JsonProperty("cost") float cost, @JsonProperty("size") Size size,
     @JsonProperty("home") boolean isHome, @JsonProperty("number") int number) {
         this.id = id;
@@ -28,31 +50,60 @@ public class Jersey {
         this.number = number;
     }
 
+    /**
+     * gets the Jersey's id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * gets the player name on the jersey
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * gets the cost of the jersey
+     * @return cost
+     */
     public float getCost(){
         return cost;
     }
 
+    /**
+     * gets the size of the jersey
+     * @return size
+     */
     public Size getSize(){
         return size;
     }
 
+    /**
+     * gets whether the colors are home or away for the jersey
+     * @return isHome?
+     */
     public boolean getIsHome(){
         return isHome;
     }
 
+    /**
+     * gets the number on the jersey
+     * @return number
+     */
     public int getNumber(){
         return number;
     }
     
     @Override
+    /**
+     * Determines whether a Jersey is equal to another jersey
+     * They are equal if they have the same id
+     * @return true if it is equal, false otherwise
+     */
     public boolean equals(Object o){
         if(o instanceof Jersey){
             Jersey object = (Jersey)(o);
@@ -64,5 +115,5 @@ public class Jersey {
         }
         return false;
     }
-    
+
 }
