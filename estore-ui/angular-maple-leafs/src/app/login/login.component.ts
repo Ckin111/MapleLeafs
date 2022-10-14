5cca5db54221983778deac6f0c951aa5e6ea5302
Ckin111 <niccollsb@gmail.com>
2022-10-13 21:38:16 -0400

    partially working login

--- estore-ui/angular-maple-leafs/src/app/login/login.component.ts
+++ estore-ui/angular-maple-leafs/src/app/login/login.component.ts
@@ -9,6 +9,31 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  loggedIn: boolean = false;
+  adminUsername: String = "admin";
+  adminPassword: String = "admin";
+  message: String = "Please Enter Username and Password";
+  enteredUsername: String= "";
+  enteredPassword: String= "";
+
+  
+  checker (htmlUsername: String, htmlPassword: String ): boolean {
+    // let username = document.getElementById('username')
+    // const username_value: String = username.value;
+    
+    if (htmlUsername==this.adminUsername && htmlPassword==this.adminPassword){
+      this.loggedIn = true;
+      this.message = "Logged In";
+      return true;
+    }else {
+
+      this.message = "Incorrect Username or Password"
+  
+      return false;
+    }
+
+  };
+
   ngOnInit(): void {
   }
 
