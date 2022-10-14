e5ca9b67ff4bd14775b380750a819bbe2802ff16
Claire Kreisel <clairekreisel@mail.rit.edu>
2022-10-13 22:27:52 -0400

    jersey equals test

--- estore-api/src/test/java/com/estore/api/estoreapi/Model/JerseyTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/Model/JerseyTest.java
@@ -1,6 +1,7 @@
 package com.estore.api.estoreapi.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -33,5 +34,24 @@ public class JerseyTest {
         assertEquals(expectedNumber,jersey.getNumber());
     }
 
-    
+    @Test
+    public void testEquals() {
+        // Setup
+        int expected_id = 7;
+        String expected_name = "Dom";
+        float expectedCost = 32.58f;
+        Size expectedSize = Size.MEDIUM;
+        boolean expectedIsHome = true;
+        int expectedNumber = 93;
+        
+
+        // Invoke
+        Jersey jersey = new Jersey(expected_id,expected_name,expectedCost,expectedSize,expectedIsHome,expectedNumber);
+        Jersey jersey2 = new Jersey(expected_id+1,expected_name,expectedCost,expectedSize,expectedIsHome,expectedNumber);
+        
+        //Analyze
+        assertEquals(jersey, jersey);
+        assertNotEquals(jersey, null);
+        assertNotEquals(jersey, jersey2);
+    }
 }
