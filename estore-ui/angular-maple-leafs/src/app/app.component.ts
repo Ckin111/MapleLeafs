5db89b81868237a0bb3c88dc9e419db726c50824
Ming Creekmore <mcreekmore480@gmail.com>
2022-10-16 12:10:35 -0400

    Updated website title and main page displays web title

--- estore-ui/angular-maple-leafs/src/app/app.component.ts
+++ estore-ui/angular-maple-leafs/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-maple-leafs';
+  title = 'Maple Leafs Jersey Store';
 }
