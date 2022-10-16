532cbc173ca765814b8fe7f769a71d5e44b06f86
Ming Creekmore <mcreekmore480@gmail.com>
2022-10-16 10:34:01 -0400

    Revert "Revert "Created view-jersey component"" Undoing stupid deleted This reverts commit 607b24e4f21a12a2945d17dec320513736ce171b.

---
+++ estore-ui/angular-maple-leafs/src/app/view-jersey/view-jersey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-jersey',
+  templateUrl: './view-jersey.component.html',
+  styleUrls: ['./view-jersey.component.css']
+})
+export class ViewJerseyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
