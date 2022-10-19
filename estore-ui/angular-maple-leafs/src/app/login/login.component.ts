5f2fccf007fdf2d6a266a9faca0f63015e7eb960
Ckin111 <niccollsb@gmail.com>
2022-10-18 21:06:35 -0400

    admin can login

--- estore-ui/angular-maple-leafs/src/app/login/login.component.ts
+++ estore-ui/angular-maple-leafs/src/app/login/login.component.ts
@@ -6,29 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  
+  isuser: boolean = false;
+  isguest: boolean = false;
+  isadmin: boolean = false;
   loggedIn: boolean = false;
   adminUsername: String = "admin";
   adminPassword: String = "admin";
   message: String = "Please Enter Username and Password";
-  enteredUsername: String= "";
-  enteredPassword: String= "";
 
-  
   checker (htmlUsername: String, htmlPassword: String ): boolean {
-    // let username = document.getElementById('username')
-    // const username_value: String = username.value;
-    
-    if (htmlUsername==this.adminUsername && htmlPassword==this.adminPassword){
+ 
+    if ( htmlUsername==this.adminUsername && htmlPassword==this.adminPassword){
+      this.isadmin = true;
       this.loggedIn = true;
       this.message = "Logged In";
       return true;
     }else {
 
       this.message = "Incorrect Username or Password"
-  
       return false;
     }
 
