3eb8e2b28559627457652661b6f3b9238678b4ad
Ckin111 <niccollsb@gmail.com>
2022-10-18 21:21:02 -0400

    guest

--- estore-ui/angular-maple-leafs/src/app/login/login.component.ts
+++ estore-ui/angular-maple-leafs/src/app/login/login.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   
   isuser: boolean = false;
-  isguest: boolean = false;
   isadmin: boolean = false;
   loggedIn: boolean = false;
   adminUsername: String = "admin";
