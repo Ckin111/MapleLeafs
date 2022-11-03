605b589cf73d75bcc1318c2e1cfb5c2454b72aae
WizardML7 <dml3483@g.rit.edu>
2022-11-02 20:49:24 -0400

    Some more routing stuff ya know

--- estore-ui/angular-maple-leafs/src/app/search-jerseys/search-jerseys.component.ts
+++ estore-ui/angular-maple-leafs/src/app/search-jerseys/search-jerseys.component.ts
@@ -23,7 +23,7 @@ export class SearchJerseysComponent implements OnInit {
    * when instantiate browse-jersey
    * @param jerseyService the service to talk to jersey API
    */
-  constructor(private jerseyService: JerseyService) { }
+  constructor(private jerseyService: JerseyService) {   }
 
   search(term: string): void {
     this.searchTerms.next(term);
