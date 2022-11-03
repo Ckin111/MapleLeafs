83eb3a615c2d0a3b4d19d4387e78666c9c5b1fbc
Ckin111 <niccollsb@gmail.com>
2022-11-03 14:43:44 -0400

    added create jersey

--- estore-ui/angular-maple-leafs/src/app/login/login.component.ts
+++ estore-ui/angular-maple-leafs/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Router, RouterModule } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,11 @@ import { Router, RouterModule } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router){}
+  constructor(private router: Router, private userService: UserService){}
+
+  tempUser:User = {
+    username: ""
+  }
 
   isuser: boolean = false;
   isadmin: boolean = false;
@@ -39,6 +45,19 @@ export class LoginComponent implements OnInit {
     }
 
   };
+
+  signup(htmlUsername: string): boolean{
+    this.tempUser.username = htmlUsername;
+    
+    if ((this.userService.addUser(this.tempUser)) == null){
+      this.message = "Username already exists";
+      return false;
+    } 
+    this.message = "Signed Up!";
+    return true;
+
+    
+  }
 
   user(username: String):void {
     // this function would redirect to browse and search
