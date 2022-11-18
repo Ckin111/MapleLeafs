eedb0d6c0870d6e834103c7f1d127095501190f8
Ming Creekmore <mcreekmore480@gmail.com>
2022-11-18 07:17:50 -0500

    Edit jersey now binds the default values to the backend

--- estore-ui/angular-maple-leafs/src/app/edit-jersey/edit-jersey.component.ts
+++ estore-ui/angular-maple-leafs/src/app/edit-jersey/edit-jersey.component.ts
@@ -20,11 +20,6 @@ export class EditJerseyComponent implements OnInit {
     number: NaN
   };
   owner: boolean = true; //TODO need to do login stuff
-  name: string = "";
-  number: number = NaN;
-  cost: number = NaN;
-  size: number = 0;
-  isHome: boolean = false;
 
   constructor(private route: ActivatedRoute,
     private jerseyService: JerseyService,
@@ -39,11 +34,6 @@ export class EditJerseyComponent implements OnInit {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     console.log("Editing");
     this.jerseyService.getJersey(id).subscribe(jersey => this.jersey = jersey);
-    this.name = this.jersey.name;
-    this.number = this.jersey.number;
-    this.cost = this.jersey.cost;
-    this.size = this.jersey.size;
-    this.isHome = this.jersey.home;
   }
 
   back(): void {
@@ -51,12 +41,6 @@ export class EditJerseyComponent implements OnInit {
   }
 
   save(): void {
-    this.jersey.name = this.name;
-    this.jersey.number = this.number;
-    this.jersey.cost = this.cost;
-    this.jersey.size = this.size;
-    this.jersey.home = this.isHome;
-
     this.jerseyService.editJersey(this.jersey).subscribe(jersey => {this.jerseys.push(jersey);});
     // this.router.navigateByUrl("view/" + this.jersey.id);
     this.back();
